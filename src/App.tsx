055561cc82aed6905b3b7dd48f6167358b88ecf9
alex-james-vukovity <alex.vukovity@gmail.com>
2021-01-30 03:33:45 +0000

    create a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import { FC, useState } from 'react'
+import { Formik, Form } from 'formik'
 
-import { Heading, Box, Button } from './components'
+import { Heading, Box, Button, TextField } from './components'
+import { AppFormProps } from './interfaces'
+import { AppFormSchema } from './validations'
 
 export const App: FC = () => {
-  const [count, setCount] = useState(0)
+  const onSubmit = async (values: AppFormProps): Promise<void> => {
+    await console.log(values)
+  }
 
   return (
     <Box>
-      <Box>
-        <Button onClick={() => setCount(count + 1)}>Increase</Button>
-      </Box>
-      <Box>
-        <Button>Im not working</Button>
-      </Box>
-      <Box>{count}</Box>
-      <Box>hey</Box>
-      <Heading variant='h1'>Hey im h1</Heading>
-      <Heading variant='h2'>Hey im h2</Heading>
-      <Heading variant='h3'>Hey im h3</Heading>
+      <Heading variant="h1">Hey im h1</Heading>
+      <Formik initialValues={{ name: '' }} validationSchema={AppFormSchema} onSubmit={onSubmit}>
+        {({ isSubmitting }) => {
+          return (
+            <Form>
+              <TextField name="name" label="Name" />
+              <Button disabled={isSubmitting} type="submit">
+                Send it
+              </Button>
+            </Form>
+          )
+        }}
+      </Formik>
     </Box>
   )
 }
