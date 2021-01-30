2e51a2273bd60f7b5a7d1e15519804ea2b3dabe7
alex-james-vukovity <alex.vukovity@gmail.com>
2021-01-30 01:46:34 +0000

    format it up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import {FC} from 'react'
+import { FC, useState } from 'react'
+
+import { Heading, Box, Button } from './components'
 
 export const App: FC = () => {
+  const [count, setCount] = useState(0)
+
   return (
-    <div >
-      hey
-    </div>
-  );
+    <Box>
+      <Box>
+        <Button onClick={() => setCount(count + 1)}>Increase</Button>
+      </Box>
+      <Box>
+        <Button>Im not working</Button>
+      </Box>
+      <Box>{count}</Box>
+      <Box>hey</Box>
+      <Heading variant='h1'>Hey im h1</Heading>
+      <Heading variant='h2'>Hey im h2</Heading>
+      <Heading variant='h3'>Hey im h3</Heading>
+    </Box>
+  )
 }
