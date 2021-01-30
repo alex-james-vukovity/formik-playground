0386134474a2a01d9aa79b1cdacce2f234e406db
alex-james-vukovity <alex.vukovity@gmail.com>
2021-01-30 16:09:49 +0000

    define get req

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from 'react'
-import { Formik, Form } from 'formik'
+import { FC, useState, useEffect } from 'react'
+import { Formik } from 'formik'
 
 import {
   Heading,
@@ -8,7 +8,9 @@ import {
   Panel,
   FormGridContainer,
   FormGridItem,
-  Text
+  Text,
+  MultiLineTextField,
+  Form
 } from './components'
 import { AppFormProps } from './interfaces'
 import { AppFormSchema } from './validations'
@@ -16,6 +18,21 @@ import { AppFormSchema } from './validations'
 export const App: FC = () => {
   const [data, setData] = useState<AppFormProps | undefined>(undefined)
   const [notification, setNotification] = useState('')
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+
+      if (response.status === 200) {
+        const responseBody = await response.json()
+        setData(responseBody)
+      } else if (response.status >= 400) {
+        setNotification('Server side error')
+      }
+    }
+
+    fetchPosts()
+  }, [])
 
   const onSubmit = async (values: AppFormProps): Promise<void> => {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
@@ -39,7 +56,8 @@ export const App: FC = () => {
     <Panel>
       <Heading variant="h1">Hey im h1</Heading>
       <Formik
-        initialValues={data || { firstName: '', lastName: '' }}
+        enableReinitialize
+        initialValues={data || ({} as AppFormProps)}
         validationSchema={AppFormSchema}
         onSubmit={onSubmit}
       >
@@ -48,14 +66,14 @@ export const App: FC = () => {
             <Form>
               <FormGridContainer>
                 <FormGridItem>
-                  <TextField name="firstName" label="First name" />
+                  <TextField name="title" label="Title" />
                 </FormGridItem>
                 <FormGridItem>
-                  <TextField name="lastName" label="Last name" />
+                  <MultiLineTextField name="body" label="Body" />
                 </FormGridItem>
                 <FormGridItem>
                   <Button disabled={isSubmitting} type="submit">
-                    Send it
+                    <Text>Send it</Text>
                   </Button>
                 </FormGridItem>
                 {notification && (
