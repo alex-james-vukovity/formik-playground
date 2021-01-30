a29ceb4281537fe28b992920f8c25e62209a113c
alex-james-vukovity <alex.vukovity@gmail.com>
2021-01-30 16:59:55 +0000

    rewrite tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+
 import { App } from './App'
+import { AppFormProps } from './interfaces'
 
 const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
-const expectedPutBody = {
-  firstName: 'John',
-  lastName: 'Doe'
+const expectedPutBody: AppFormProps = {
+  id: '1',
+  title: 'Fake title',
+  body: 'Fake body'
 }
 
-const server = setupServer(
-  rest.put(url, (req, res, ctx) => {
-    if (JSON.stringify(req.body) === JSON.stringify(expectedPutBody)) {
-      return res(ctx.status(200), ctx.json(expectedPutBody))
-    }
-    return res(ctx.status(404))
-  })
-)
+const expectedGetBody: AppFormProps = {
+  id: '1',
+  title: 'Bogus title',
+  body: 'Bogus body'
+}
+
+const server = setupServer()
 
 beforeAll(() =>
   server.listen({
@@ -27,17 +29,36 @@ beforeAll(() =>
   })
 )
 afterEach(() => server.resetHandlers())
+beforeEach(() => {
+  server.use(rest.get(url, (req, res, ctx) => res(ctx.status(200), ctx.json(expectedGetBody))))
+})
 afterAll(() => server.close())
 
 test('should render the heading', async () => {
   render(<App />)
 
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
+
   const heading = screen.getByRole('heading', { name: 'Hey im h1' })
   expect(heading).toBeInTheDocument()
 })
 
+test('should render values from the response body ', async () => {
+  render(<App />)
+
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
+
+  const title = screen.getByRole('textbox', { name: 'Title' })
+  expect(title).toHaveValue('Bogus title')
+
+  const body = screen.getByRole('textbox', { name: 'Body' })
+  expect(body).toHaveValue('Bogus body')
+})
+
 test('should render the button', async () => {
   render(<App />)
+
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
 
   const button = screen.getByRole('button', { name: 'Send it' })
   expect(button).toBeInTheDocument()
@@ -45,13 +66,26 @@ test('should render the button', async () => {
 })
 
 test('should render the success notification after form submission', async () => {
+  server.use(
+    rest.put(url, (req, res, ctx) => {
+      if (JSON.stringify(req.body) === JSON.stringify(expectedPutBody)) {
+        return res(ctx.status(200), ctx.json(expectedPutBody))
+      }
+      return res(ctx.status(404))
+    })
+  )
+
   render(<App />)
 
-  const firstName = await screen.findByRole('textbox', { name: 'First name' })
-  userEvent.type(firstName, 'John')
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
 
-  const lastName = await screen.findByRole('textbox', { name: 'Last name' })
-  userEvent.type(lastName, 'Doe')
+  const title = await screen.findByRole('textbox', { name: 'Title' })
+  userEvent.clear(title)
+  userEvent.type(title, 'Fake title')
+
+  const body = await screen.findByRole('textbox', { name: 'Body' })
+  userEvent.clear(body)
+  userEvent.type(body, 'Fake body')
 
   const button = await screen.findByRole('button', { name: 'Send it' })
   expect(button).toBeInTheDocument()
@@ -71,11 +105,15 @@ test('should render server side error', async () => {
 
   render(<App />)
 
-  const firstName = await screen.findByRole('textbox', { name: 'First name' })
-  userEvent.type(firstName, 'John')
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
 
-  const lastName = await screen.findByRole('textbox', { name: 'Last name' })
-  userEvent.type(lastName, 'Doe')
+  const title = await screen.findByRole('textbox', { name: 'Title' })
+  userEvent.clear(title)
+  userEvent.type(title, 'Fake title')
+
+  const body = await screen.findByRole('textbox', { name: 'Body' })
+  userEvent.clear(body)
+  userEvent.type(body, 'Fake body')
 
   const button = await screen.findByRole('button', { name: 'Send it' })
   expect(button).toBeInTheDocument()
@@ -91,7 +129,21 @@ test('should render server side error', async () => {
 })
 
 test('should render client side validation errors', async () => {
+  server.use(
+    rest.put(url, (req, res, ctx) => {
+      if (JSON.stringify(req.body) === JSON.stringify(expectedPutBody)) {
+        return res(ctx.status(200), ctx.json(expectedPutBody))
+      }
+      return res(ctx.status(404))
+    })
+  )
+
   render(<App />)
+
+  await waitForElementToBeRemoved(() => screen.getByRole('spinbutton'))
+
+  const title = await screen.findByRole('textbox', { name: 'Title' })
+  userEvent.clear(title)
 
   const button = await screen.findByRole('button', { name: 'Send it' })
   expect(button).toBeInTheDocument()
